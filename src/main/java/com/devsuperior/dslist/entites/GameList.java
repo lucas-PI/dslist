5827b7b21741f89af0df712087113b053a3f0b4c
lucas-PI <lucas22inacio@hotmail.com>
2025-05-23 15:32:27 -0300

    created class GameList

---
+++ src/main/java/com/devsuperior/dslist/entites/GameList.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dslist.entites;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public GameList() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
