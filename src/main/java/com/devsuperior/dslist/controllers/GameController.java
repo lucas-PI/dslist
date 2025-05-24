0a2467513cae67586d1f4aa95cb799dab2609186
lucas-PI <lucas22inacio@hotmail.com>
2025-05-23 22:05:22 -0300

    add methody find by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.entites.Game;
+import com.devsuperior.dslist.entites.dto.GameMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dslist.services.GameService;
@@ -17,9 +19,14 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping
-    public ResponseEntity<List<Game>> findAll(){
-        List<Game> gamerList = gameService.findALL();
-        return ResponseEntity.ok().body(gamerList);
+    public ResponseEntity<List<GameMinDTO>> findAll(){
+        List<GameMinDTO> gameList = gameService.findALL();
+        return ResponseEntity.ok().body(gameList);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Game> findById(@PathVariable Long id){
+        Game obj = gameService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
